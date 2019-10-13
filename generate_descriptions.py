e77a9b6a7c12b0f6bb13cd7cf51565908481f370
root <winson.luk@gmail.com>
2019-10-13 14:46:17 -0700

    update

--- generate_descriptions.py
+++ generate_descriptions.py
@@ -8,13 +8,15 @@ with open('io/pitches.txt') as f:
     gc.collect()
     for line in f:
         line = line[0].lower() + line[1:]
-        line = 'Dotino is a technology company. Dotino\'s mission is ' + line.strip()
-        res = gpt2.generate(sess,
-                length=120,
-                top_p=0.25,
-                temperature=1.2,
-                prefix=line,
-                return_as_list=True,
-                )
+        line = 'Dotino is a technology company. Dotino\'s mission is ' + line.strip() + '.'
+        res = ['million']
+        while 'funding' in res[0] or 'million' in res[0]:
+            res = gpt2.generate(sess,
+                    length=120,
+                    top_p=0.25,
+                    temperature=1.2,
+                    prefix=line,
+                    return_as_list=True,
+                    )
         with open('io/descriptions.txt', 'a+') as g:
             g.write(res[0][32:].replace('\n', ' ').rsplit('.', 1)[0].strip() + '.\n')
