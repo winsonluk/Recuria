a75cc3a384004ffa4f41123d49834ab8b7b90844
root <winson.luk@gmail.com>
2019-10-13 19:46:27 +0000

    update

---
+++ generate_descriptions.py
@@ -0,0 +1,18 @@
+import gpt_2_simple as gpt2
+
+sess = gpt2.start_tf_sess()
+gpt2.load_gpt2(sess)
+
+with open('io/pitches.txt') as f:
+    for line in f:
+        line = line[0].lower() + line[1:]
+        line = 'Dotino is a technology company. Dotino\'s mission is ' + line.strip()
+        res = gpt2.generate(sess,
+                length=120,
+                top_p=0.25,
+                temperature=1.2,
+                prefix=line,
+                return_as_list=True,
+                )
+        with open('io/descriptions.txt') as g:
+            g.write(res[0][32:].replace('\n', ' ').rsplit('.', 1)[0].strip() + '.')
