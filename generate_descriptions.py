1ab6494d52abf6603196da4f07219b769f54bcc3
root <winson.luk@gmail.com>
2019-10-21 12:41:49 +0000

    update

--- generate_descriptions.py
+++ generate_descriptions.py
@@ -5,32 +5,27 @@ import gpt_2_simple as gpt2
 sess = gpt2.start_tf_sess()
 gpt2.load_gpt2(sess)
 
-START = 538
+START = 1
 
-with open('io/pitches.txt') as f, open('io/names.txt') as f2:
+with open('io/summaries.txt') as f, open('io/names.txt') as f2:
     counter = 0
     for line, line2 in zip(f, f2):
         counter += 1
         if counter < START:
             continue
-#        if counter % 10 == 0:
-#            sess = gpt2.reset_session(sess)
-#            gpt2.load_gpt2(sess)
-        line = line[0].lower() + line[1:]
-        line2 = line2[:-1]
-        line = line2 + ' is a technology company. ' + line2 + '\'s mission is ' + line.strip() + '.'
+        line = line2.strip() + ' is a technology company. ' + line.strip()
         res = gpt2.generate(sess,
-                length=100,
+                length=140,
                 top_p=0.2,
                 temperature=1.3,
                 prefix=line,
                 return_as_list=True,
                 )
-        res[0] = res[0][len(line2) + 26:].replace('\n', ' ').rsplit('.', 1)[0].strip()
+        res[0] = res[0][len(line2) + 25:].replace('\n', ' ').rsplit('.', 1)[0].strip()
         if bool(re.search('\$\d+$', res[0])):
             res[0] = res[0] + '8 million'
-        res[0] = res[0] +  '.\n'
-        with open('io/descriptions.txt', 'a+') as g:
+        res[0] = res[0].replace('<|endoftext|>', ' ') +  '.\n'
+        with open('io/descriptions_with_summaries.txt', 'a+') as g:
             g.write(res[0])
         del line
         del line2
