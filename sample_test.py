374d677b6d2e8c3c6e4fb45f09d5ee56000ff535
root <winson.luk@gmail.com>
2019-10-23 03:59:38 +0000

    update

--- sample_test.py
+++ sample_test.py
@@ -10,10 +10,9 @@ for i in [x * 0.1 for x in range(0, 9) if (START_P + x * 0.1) <= 1]:
     for j in [x * 0.2 for x in range(0, 10)]:
         print('top_p: ' + str(START_P + i) + '\ntemp: ' + str(START_T + j))
         gpt2.generate(sess,
-                length=180,
-                nsamples=5,
+                length=100,
+                nsamples=3,
                 top_p=START_P + i,
                 temperature=START_T + j,
-                prefix="Dotino is a SaaS Project Management Software Compiler.",
+                prefix="PeopleLink's mission is delivering anything anytime without email. PeopleLink is the new social email app, now also using our SMS delivery app.  Our objective is bring people together, no matter where you or they are.",
                 )
-
