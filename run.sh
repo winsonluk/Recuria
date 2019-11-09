77f1449d4236b1c45569a9434385c25b303729a7
root <winson.luk@gmail.com>
2019-11-09 16:20:58 +0000

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 while true; do
-  /usr/bin/pkill -f run.sh; /usr/bin/pkill -f python; cd ~/gpt_descriptions
+  # kill $(pgrep -x run.sh | grep -v ^$$$)
+  /usr/bin/pkill -f python
+  cd ~/gpt_descriptions
   git commit -am "update" && git push --set-upstream origin master
   nohup nice -n -20 python ~/gpt_descriptions/generate_descriptions.py $(($(wc -l ~/gpt_descriptions/io/descriptions.txt | cut -f1 -d' ') + 1))
 done &
