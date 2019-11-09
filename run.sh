0a8caa390cf8d19640e088ccb507fcc523966495
root <winson.luk@gmail.com>
2019-11-09 16:36:21 +0000

    update

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 while true; do
   git commit -am "update" && git push --set-upstream origin master
   nohup python ~/gpt_descriptions/generate_descriptions.py $(($(wc -l ~/gpt_descriptions/io/descriptions.txt | cut -f1 -d' ') + 1)) &
-  sleep 60
+  sleep 3600
   /usr/bin/pkill -f python
 done
