1abe902dbf3f3816149e21d23016131a3467b0ac
root <winson.luk@gmail.com>
2019-11-09 03:46:29 +0000

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 while true; do
-  git commit -am "update";git push
+  git commit -am "update";git push --set-upstream origin master
   nohup python ~/gpt_descriptions/generate_descriptions.py $(($(wc -l ~/gpt_descriptions/io/descriptions.txt | cut -f1 -d' ') + 1))
 done &
